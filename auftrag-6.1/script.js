0c2b6f2144081fe5d62418508394d3490f1413b8
Robin Bühler <git+r@obin.ch>
2023-01-19 17:10:58 +0100

    feat: add 6.1

--- auftrag-6.1/script.js
+++ auftrag-6.1/script.js
@@ -2,7 +2,22 @@ const canvas = document.getElementById('smiley');
 const context = canvas.getContext("2d");
 
 // Kopf zeichnen
+context.beginPath();
+context.arc(95, 85, 40, 0, 2 * Math.PI);
+context.fillStyle = "yellow";
+context.fill();
+context.closePath();
 
 // Augen zeichnen
+context.beginPath();
+context.arc(85, 75, 5, 0, 2 * Math.PI);
+context.arc(105, 75, 5, 0, 2 * Math.PI);
+context.fillStyle = "black";
+context.fill();
+context.closePath();
 
 // Mund zeichnen
+context.beginPath();
+context.arc(95, 90, 20, 0, Math.PI, false);
+context.stroke();
+context.closePath();
