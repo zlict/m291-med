611de94ab646f7c231dda7c7b7f581acb492c697
Robin Bühler <git+r@obin.ch>
2023-01-26 11:29:35 +0100

    feat: add 6.3

--- auftrag-6.3/script.js
+++ auftrag-6.3/script.js
@@ -1,6 +1,26 @@
 const canvas = document.getElementById('watch');
 const context = canvas.getContext("2d");
 
-// Bild laden
+const image = new Image();
+image.src = 'cleanwatch.jpg';
 
-// Zeit einzeichnen
+const font = new FontFace('segments', 'url(7segments.ttf)');
+
+// Zeichnen
+function draw() {
+  if (image.complete) {
+    const dateTime = new Date();
+    context.drawImage(image, 0, 0);
+    context.font = "46px segments"
+    context.fillStyle = "#717482";
+    context.fillText(`${dateTime.getHours().toString().padStart(2, 0)}:${dateTime.getMinutes().toString().padStart(2, 0)}:${dateTime.getSeconds().toString().padStart(2, 0)}`, 410, 550);
+    setTimeout(draw, 1000);
+  }
+}
+
+// Zeichnen, wenn Bild oder Font geladen ist
+image.addEventListener('load', draw);
+font.load().then((f) => {
+  document.fonts.add(f);
+  draw();
+});
